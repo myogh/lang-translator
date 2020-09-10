9014b4d7baeb1102c4a52b6531cdfdf67157c960
iamamh <aungmyohtet.mlr@gmail.com>
2020-09-10 10:23:17 +0630

    Updates

---
+++ precache-manifest.bda1ad478898bca2835d1ece587b1015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703e452ff54d0e22a39c070998f588a8",
+    "url": "/lang-translator/index.html"
+  },
+  {
+    "revision": "ed451b9748fa03440864",
+    "url": "/lang-translator/static/css/main.012e695d.chunk.css"
+  },
+  {
+    "revision": "331a392b2b14c783500a",
+    "url": "/lang-translator/static/js/2.eaec2e1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lang-translator/static/js/2.eaec2e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed451b9748fa03440864",
+    "url": "/lang-translator/static/js/main.a26b9722.chunk.js"
+  },
+  {
+    "revision": "da997d087dadcd524de4",
+    "url": "/lang-translator/static/js/runtime-main.3991d5c9.js"
+  }
+]);
